63102cb91c3f234b2dc13c1e7ddd33dccc05da37
Lucas Stellet <lustepe@gmail.com>
2022-09-23 15:57:05 -0300

    rename component

--- lib/todo_web/components/content/task_card.ex
+++ lib/todo_web/components/content/task_card.ex
@@ -1,17 +1,37 @@
 defmodule TodoWeb.Components.Content.TaskCard do
   use TodoWeb, :component
 
-  attr :task_card, :string
+  attr :button, :string
+  attr :input, :string
+  attr :card, :string
 
-  slot(:inner_block, required: true)
+  slot(:inner_block, required: false)
 
   def task_card(assigns) do
     ~H"""
-    <div class="m-3">
-      <h1>
-        <%= render_slot(@inner_block) %>
-      </h1>
+    <div class={@card}>
+      <div class="p-4">
+        <button class={@button}></button>
+      </div>
+
+      <%= if Enum.count(@inner_block) > 0  do %>
+        <div class={@input}>
+          <p><%= render_slot(@inner_block) %></p>
+        </div>
+      <% else %>
+        <form phx-submit="create_task" class="flex-1">
+          <input class={@input} type="text" name="description" placeholder="Create a new todo..." />
+        </form>
+      <% end %>
     </div>
     """
   end
 end
+
+# <input
+# class={@input}
+# type="text"
+# name="description"
+# value={render_slot(@inner_block)}
+# disabled={false}
+# />
