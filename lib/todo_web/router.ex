46e1a6f003a91f484d45d0dda35f6fe5d0b2c341
Lucas Stellet <lustepe@gmail.com>
2022-09-22 22:33:37 -0300

    add live route

--- lib/todo_web/router.ex
+++ lib/todo_web/router.ex
@@ -17,7 +17,7 @@ defmodule TodoWeb.Router do
   scope "/", TodoWeb do
     pipe_through :browser
 
-    get "/", PageController, :index
+    live "/", HomeLive
   end
 
   # Other scopes may use custom stacks.
