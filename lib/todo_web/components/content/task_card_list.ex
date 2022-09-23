dfc3839412b2d50dd45b65f0c7a851bb87ee3a59
Lucas Stellet <lustepe@gmail.com>
2022-09-23 15:55:51 -0300

    create task_card _list component

---
+++ lib/todo_web/components/content/task_card_list.ex
@@ -0,0 +1,46 @@
+defmodule TodoWeb.Components.Content.TaskCardList do
+  use TodoWeb, :component
+
+  attr :tasks, :list
+  attr :card_list, :string
+  attr :card, :string
+  attr :bottom, :string
+  attr :button, :string
+  attr :input, :string
+
+  import TodoWeb.Components.Content.TaskCard
+
+  def task_card_list(assigns) do
+    ~H"""
+    <div class={@card_list}>
+      <%= for task <- @tasks do %>
+        <.task_card input={@input} card={@card} button={@button}>
+          <%= task.description %>
+        </.task_card>
+      <% end %>
+      <div class={@bottom}>
+        <button class="text-dark-mode-bottom-text text-[14px] font-bold">
+          5 items left
+        </button>
+
+        <div class="mx-2 flex-1 flex justify-evenly">
+          <button class="text-dark-mode-bottom-text text-[14px] font-bold">
+            All
+          </button>
+
+          <button class="text-dark-mode-bottom-text text-[14px] font-bold">
+            Active
+          </button>
+
+          <button class="text-dark-mode-bottom-text text-[14px] font-bold">
+            Completed
+          </button>
+        </div>
+        <button class="font-bold text-dark-mode-bottom-text text-[14px]">
+          Clear Completed
+        </button>
+      </div>
+    </div>
+    """
+  end
+end
