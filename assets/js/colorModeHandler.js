b6b0a5a2ed28b7d835946ac155e9b2eccf0640db
Lucas Stellet <lustepe@gmail.com>
2022-09-23 22:54:58 -0300

    change light background color

--- assets/js/colorModeHandler.js
+++ assets/js/colorModeHandler.js
@@ -12,7 +12,7 @@ export function changeToDarkMode() {
 }
 
 export function changeToLightMode() {
-  document.getElementById('todo-app').style.backgroundColor = '#f9f9f9';
+  document.getElementById('todo-app').style.backgroundColor = '#eaeaea';
   setColor('light');
 }
 
