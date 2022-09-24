545f54a032a7dd9f394f6b05e8793f7bc8982dcc
Lucas Stellet <lustepe@gmail.com>
2022-09-24 01:14:22 -0300

    add ex_heroicons lib

--- mix.exs
+++ mix.exs
@@ -39,7 +39,7 @@ defmodule Todo.MixProject do
       {:postgrex, ">= 0.0.0"},
       {:phoenix_html, "~> 3.0"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
-      {:phoenix_live_view, github: "phoenixframework/phoenix_live_view"},
+      {:phoenix_live_view, github: "phoenixframework/phoenix_live_view", override: true},
       {:floki, ">= 0.30.0", only: :test},
       {:esbuild, "~> 0.4", runtime: Mix.env() == :dev},
       {:telemetry_metrics, "~> 0.6"},
@@ -47,7 +47,8 @@ defmodule Todo.MixProject do
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
       {:tailwind, "~> 0.1", runtime: Mix.env() == :dev},
-      {:dialyxir, "~> 1.0", only: [:dev, :test], runtime: false}
+      {:dialyxir, "~> 1.0", only: [:dev, :test], runtime: false},
+      {:ex_heroicons, "~> 2.0.0"}
     ]
   end
 
