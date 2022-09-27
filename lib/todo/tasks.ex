366a8289b24282d94552596caafaff5da262befd
Lucas Stellet <lustepe@gmail.com>
2022-09-27 10:55:22 -0300

    add docs to functions

--- lib/todo/tasks.ex
+++ lib/todo/tasks.ex
@@ -9,15 +9,19 @@ defmodule Todo.Tasks do
   alias Todo.Tasks.Task
 
   @doc """
-  Returns the list of tasks.
+  Returns tasks from author..
 
   ## Examples
 
-  iex> list_tasks()
+  iex> list_tasks_by_author("author")
   [%Task{}, ...]
+
+  iex> list_tasks_by_author("unknown author")
+  nil
 
   """
 
+  @spec list_tasks_by_author(Ecto.UUID.t()) :: [Task.t()] | nil
   def list_tasks_by_author(author) do
     query =
       from t in Task,
@@ -28,6 +32,20 @@ defmodule Todo.Tasks do
     Repo.all(query)
   end
 
+  @doc """
+  Returns tasks bases on status.
+
+  ## Examples
+
+  iex> list_tasks_by_status("author", "status")
+  [%Task{}, ...]
+
+  iex> list_tasks_by_status("author", "uunknown status")
+  nil
+
+  """
+
+  @spec list_tasks_by_status(Ecto.UUID.t(), atom()) :: [Task.t()] | nil
   def list_tasks_by_status(author, status) do
     query =
       from t in Task,
@@ -38,24 +56,62 @@ defmodule Todo.Tasks do
     Repo.all(query)
   end
 
+  @doc """
+  Change status `:completed` from completed to `:deleted`
+
+  ## Examples
+
+  iex> delete_completed_tasks("author")
+  [%Task{}, ...]
+
+  """
+
+  @spec delete_completed_tasks(Ecto.UUID.t()) :: :ok | {:error, atom()}
   def delete_completed_tasks(author) do
     query =
       from t in Task,
         where: t.author == ^author and t.status == :completed,
         select: t
 
-    Repo.update_all(query, set: [status: :deleted])
+    case Repo.update_all(query, set: [status: :deleted]) do
+      {_, [%Task{} | _]} ->
+        :ok
+
+      {0, []} ->
+        {:error, :no_completed_tasks}
+    end
   end
 
+  @spec delete_task(Ecto.UUID.t(), Ecto.UUID.t()) :: :ok | {:error, atom()}
   def delete_task(author, task_id) do
     query =
       from t in Task,
         where: t.author == ^author and t.id == ^task_id,
         select: t
 
-    Repo.update_all(query, set: [status: :deleted])
+    case Repo.update_all(query, set: [status: :deleted]) do
+      {1, [%Task{id: ^task_id}]} ->
+        :ok
+
+      {0, []} ->
+        {:error, :task_not_found}
+    end
   end
 
+  @doc """
+  Change task status to completed.
+
+  ## Examples
+
+      iex> complete_task("author_id", "task_id")
+      {:ok}
+
+      iex> complete_task("author_id", "wrong_task_is")
+      {:error, :task_not_found}
+
+  """
+
+  @spec complete_task(Ecto.UUID.t(), Ecto.UUID.t()) :: :ok | {:error, atom()}
   def complete_task(author, task_id) do
     query =
       from t in Task,
@@ -65,13 +121,32 @@ defmodule Todo.Tasks do
     Repo.update_all(query, set: [status: :completed])
   end
 
+  @doc """
+  Change task status between completed and active.
+
+  ## Examples
+
+      iex> toggle_task_status("author_id", "task_id", "active" | "completed")
+      {:ok}
+
+      iex> toggle_task_status("author_id", "wrong_task_is", "wrong_status")
+      {:error, :task_not_found}
+
+  """
+  @spec toggle_task_status(Ecto.UUID.t(), Ecto.UUID.t(), binary()) :: :ok | {:error, atom()}
   def toggle_task_status(author, task_id, "active") do
     query =
       from t in Task,
         where: t.author == ^author and t.id == ^task_id,
         select: t
 
-    Repo.update_all(query, set: [status: :completed])
+    case Repo.update_all(query, set: [status: :completed]) do
+      {1, [%Task{id: ^task_id}]} ->
+        :ok
+
+      {0, []} ->
+        {:error, :task_not_found}
+    end
   end
 
   def toggle_task_status(author, task_id, "completed") do
@@ -80,7 +155,13 @@ defmodule Todo.Tasks do
         where: t.author == ^author and t.id == ^task_id,
         select: t
 
-    Repo.update_all(query, set: [status: :active])
+    case Repo.update_all(query, set: [status: :active]) do
+      {1, [%Task{id: ^task_id}]} ->
+        :ok
+
+      {0, []} ->
+        {:error, :task_not_found}
+    end
   end
 
   @doc """
